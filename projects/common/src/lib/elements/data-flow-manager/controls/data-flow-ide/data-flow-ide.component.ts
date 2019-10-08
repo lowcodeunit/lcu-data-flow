45b06f064222d2f04d7a3b17a600ae74b7c055f1
mcgear <michael.gearhardt@fathym.com>
2019-10-08 13:34:13 -0600

    Rollback

--- projects/common/src/lib/elements/data-flow-manager/controls/data-flow-ide/data-flow-ide.component.ts
+++ projects/common/src/lib/elements/data-flow-manager/controls/data-flow-ide/data-flow-ide.component.ts
@@ -3,7 +3,7 @@ import { LCUElementContext, LcuElementComponent } from '@lcu/common';
 import { DataFlowManagerState } from '../../../../core/data-flow-manager-state.model';
 import { DataFlowManagerStateManagerContext } from '../../../../core/data-flow-manager-state-manager.context';
 import { jsPlumbSurfaceComponent, AngularViewOptions, jsPlumbService } from 'jsplumbtoolkit-angular';
-import { Surface, jsPlumbToolkit, Dialogs, DrawingTools, jsPlumbUtil, LayoutSpec, SurfaceMode, jsPlumbToolkitOptions, SurfaceRenderParams } from 'jsplumbtoolkit';
+import { Surface, jsPlumbToolkit, Dialogs, DrawingTools, jsPlumbUtil, LayoutSpec, SurfaceMode } from 'jsplumbtoolkit';
 import { DataFlowModuleComponent } from '../data-flow-module/data-flow-module.component';
 import { DataFlowJSPlumbToolkitIOService } from '../../../../services/data-flow-jsplumb-toolkit-io.service';
 
@@ -28,7 +28,22 @@ export class LcuDataFlowDataFlowIdeElementComponent extends LcuElementComponent<
   protected toolkit: jsPlumbToolkit;
 
   //  Properties
-  public RenderParams: SurfaceRenderParams;
+  public RenderParams = {
+    layout: {
+      type: 'Spring'
+    },
+    events: {
+      edgeAdded: (params: any) => {
+        if (params.addedByMouse) {
+          this.EditLabel(params.edge);
+        }
+      }
+    },
+    consumeRightClick: false,
+    dragOptions: {
+      filter: '.jtk-draw-handle, .node-action, .node-action i, .bank-module'
+    }
+  };
 
   public SelectMode: SurfaceMode;
 
@@ -37,9 +52,98 @@ export class LcuDataFlowDataFlowIdeElementComponent extends LcuElementComponent<
 
   public State: DataFlowManagerState;
 
-  public ToolkitParams: jsPlumbToolkitOptions;
+  public ToolkitParams = {
+    nodeFactory: (type: string, data: any, callback: (data: object) => void) => {
+      Dialogs.show({
+        id: 'dlgText',
+        title: 'Enter ' + type + ' name:',
+        onOK: (d: any) => {
+          data.text = d.text;
+          // if the user entered a name...
+          if (data.text) {
+            // and it was at least 2 chars
+            if (data.text.length >= 2) {
+              // set an id and continue.
+              data.id = jsPlumbUtil.uuid();
 
-  public View: AngularViewOptions;
+              callback(data);
+            } else {
+              alert(type + ' names must be at least 2 characters!');
+            }
+          }
+          // else...do not proceed.
+        }
+      });
+    },
+    beforeStartConnect: (node: any, edgeType: string) => {
+      return { label: '...' };
+    }
+  };
+
+  public View: AngularViewOptions = {
+    nodes: {},
+    edges: {
+      default: {
+        anchor: 'AutoDefault',
+        endpoint: 'Blank',
+        connector: ['Flowchart', { cornerRadius: 5 }],
+        paintStyle: { strokeWidth: 2, stroke: 'rgb(132, 172, 179)', outlineWidth: 3, outlineStroke: 'transparent' },
+        hoverPaintStyle: { strokeWidth: 2, stroke: 'rgb(67,67,67)' }, // hover paint style for this edge type.
+        events: {
+          dblclick: (params: any) => {
+            Dialogs.show({
+              id: 'dlgConfirm',
+              data: {
+                msg: 'Delete Edge'
+              },
+              onOK: () => {
+                this.RemoveEdge(params.edge);
+              }
+            });
+          }
+        },
+        overlays: [['Arrow', { location: 1, width: 10, length: 10 }], ['Arrow', { location: 0.3, width: 10, length: 10 }]]
+      },
+      connection: {
+        parent: 'default',
+        overlays: [
+          [
+            'Label',
+            {
+              label: '${label}',
+              events: {
+                click: (params: any) => {
+                  this.EditLabel(params.edge);
+                }
+              }
+            }
+          ]
+        ]
+      }
+    },
+    ports: {
+      start: {
+        endpoint: 'Blank',
+        anchor: 'Continuous',
+        uniqueEndpoint: true,
+        edgeType: 'default'
+      },
+      source: {
+        endpoint: 'Blank',
+        paintStyle: { fill: '#84acb3' },
+        anchor: 'AutoDefault',
+        maxConnections: -1,
+        edgeType: 'connection'
+      },
+      target: {
+        maxConnections: -1,
+        endpoint: 'Blank',
+        anchor: 'AutoDefault',
+        paintStyle: { fill: '#84acb3' },
+        isTarget: true
+      }
+    }
+  };
 
   //  Constructors
   constructor(
@@ -63,15 +167,7 @@ export class LcuDataFlowDataFlowIdeElementComponent extends LcuElementComponent<
       renderer: this.surface
     });
 
-    this.RenderParams = this.io.LoadRenderParams(this.toolkit);
-
-    this.ToolkitParams = this.io.LoadToolkitParams(this.toolkit);
-
-    this.View = this.io.LoadView(this.toolkit);
-
-    this.io.SetViewNodes(this.toolkit, this.State.ModuleOptions, this.View);
-
-    this.Relayout();
+    this.io.LoadOntoSurface(this.surface, this.State.ActiveDataFlow.Output);
   }
 
   public ngOnDestroy() {
@@ -101,6 +197,22 @@ export class LcuDataFlowDataFlowIdeElementComponent extends LcuElementComponent<
     this.state.DeployDataFlow(this.State.ActiveDataFlow.Lookup);
   }
 
+  public EditLabel(edge: any) {
+    Dialogs.show({
+      id: 'dlgText',
+      data: {
+        text: edge.data.label || ''
+      },
+      onOK: (data: any) => {
+        this.toolkit.updateEdge(edge, { label: data.text });
+      }
+    });
+  }
+
+  public RemoveEdge(edge: any) {
+    this.toolkit.removeEdge(edge);
+  }
+
   public Relayout() {
     if (this.surface && this.State.ActiveDataFlow) {
       this.io.LoadOntoSurface(this.surface, this.State.ActiveDataFlow.Output);
@@ -115,6 +227,10 @@ export class LcuDataFlowDataFlowIdeElementComponent extends LcuElementComponent<
     this.state.SaveDataFlow(this.State.ActiveDataFlow);
   }
 
+  public ToggleSelection(node: any) {
+    this.toolkit.toggleSelection(node);
+  }
+
   public ToggleSelectMode() {
     this.SelectMode = this.SelectMode === 'pan' ? 'select' : 'pan';
 
@@ -127,7 +243,26 @@ export class LcuDataFlowDataFlowIdeElementComponent extends LcuElementComponent<
 
   //  Helpers
   protected handleStateChanged() {
-    this.io.SetViewNodes(this.toolkit, this.State.ModuleOptions, this.View);
+    this.View.nodes = {
+      selectable: {
+        events: {
+          tap: (params: any) => {
+            this.ToggleSelection(params.node);
+          }
+        }
+      }
+    };
+
+    if (this.State.ModuleOptions) {
+      this.State.ModuleOptions.forEach(option => {
+        this.View.nodes[option.ModuleType] = {
+          parent: 'selectable',
+          component: DataFlowModuleComponent
+        };
+      });
+
+      console.log(this.View.nodes);
+    }
 
     this.Relayout();
   }
