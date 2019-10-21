24267e554b61848ed4ae02b93f3fdbc848cae602
xchanin <me@shannonbruns.com>
2019-10-21 15:50:55 -0600

    merge conflict fix

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
-<<<<<<< HEAD
-import {OverlayContainer} from '@angular/cdk/overlay';
-
-import { Subscription } from 'rxjs';
-import { Dialogs } from 'jsplumbtoolkit';
-=======
 import { Router, ActivatedRoute } from '@angular/router';
 import { OverlayContainer } from '@angular/cdk/overlay';
 import { LazyElementConfig } from '@lowcodeunit/lazy-element';
 
 import { Subscription } from 'rxjs';
 import { LCUServiceSettings } from '@lcu/common';
->>>>>>> d491d0af9ed8a98a9e6f839fc7776bb89adfaf00
 
 @Component({
   selector: 'lcu-root',
@@ -29,22 +22,8 @@ export class AppComponent implements OnInit {
 
   public title = 'demo';
 
-<<<<<<< HEAD
-  protected themesSubscriptions: Subscription;
-
-  constructor(
-    protected overlayContainer: OverlayContainer) {
-
-    this.BackgroundImage = './assets/images/bg_image.jpg';
-  }
-
-  public ngOnInit(): void {
-   
-  }
-=======
   constructor(protected overlayContainer: OverlayContainer, protected settings: LCUServiceSettings) {
     this.BackgroundImage = './assets/images/bg_image.jpg';
->>>>>>> d491d0af9ed8a98a9e6f839fc7776bb89adfaf00
 
     this.Config = {
       Assets: ['https://www.fathym-int.com/_lcu/lcu-data-flow/wc/lcu-data-flow.lcu.js'],
