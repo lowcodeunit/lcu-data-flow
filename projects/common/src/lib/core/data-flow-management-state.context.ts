1f4ba0efe94bad72544261664b127acf186860ed
mcgear <michael.gearhardt@gmail.com>
2020-04-01 12:00:12 -0600

    Flip back

--- projects/common/src/lib/core/data-flow-management-state.context.ts
+++ projects/common/src/lib/core/data-flow-management-state.context.ts
@@ -78,7 +78,7 @@ export class DataFlowManagementStateContext extends StateContext<DataFlowManagem
   }
 
   protected loadStateKey() {
-    return 'data-flow';
+    return 'main';
   }
 
   protected loadStateName() {
